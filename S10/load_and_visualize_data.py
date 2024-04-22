84a1828aaa742c5cb1f7feff487df4eebaa8fa5a
das91t70 <161017685+das91t70@users.noreply.github.com>
2024-04-22 12:32:40 +0530

    Update load_and_visualize_data.py

--- S10/load_and_visualize_data.py
+++ S10/load_and_visualize_data.py
@@ -1,6 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torchvision.utils import make_grid
+import numpy as np
 import matplotlib.pyplot as plt
 class CIFAR10DataOps:
 
+  # register CIFAR10 dataset as either train or test data
+  def __init__(self, train):
+    if train:
+      self.dataset = datasets.CIFAR10('./data', train=train, download=True, transform=Tranforms(self.get_train_transforms()))
+    else:
+      self.dataset = datasets.CIFAR10('./data', train=train, download=True, transform=Tranforms(self.get_test_transforms()))
+
+  # get transformations used for training dataset
   def get_train_transforms(self):
       return albumentations.Compose([
     albumentations.PadIfNeeded(min_height=36, min_width=36, p=1),
@@ -10,22 +25,19 @@ class CIFAR10DataOps:
     albumentations.augmentations.transforms.Normalize((0.49139968, 0.48215827 ,0.44653124,), (0.24703233,0.24348505,0.26158768,)),
         albumentations.pytorch.transforms.ToTensorV2()
     ])
+
+  # get transformations used for testing dataset
   def get_test_transforms(self):
       return albumentations.Compose([
         albumentations.augmentations.transforms.Normalize((0.49139968, 0.48215827 ,0.44653124,), (0.24703233,0.24348505,0.26158768,)),
         albumentations.pytorch.transforms.ToTensorV2()
         ])
-  def __init__(self, train):
-    if train:
-      self.dataset = datasets.CIFAR10('./data', train=train, download=True, transform=Tranforms(self.get_train_transforms()))
-    else:
-      self.dataset = datasets.CIFAR10('./data', train=train, download=True, transform=Tranforms(self.get_test_transforms()))
 
-  
+  # load train/test dataset with dataloader arguments
   def load_dataset(self, dataloader_args):
     data_loader = torch.utils.data.DataLoader(self.dataset, **dataloader_args)
     return data_loader
-
+  # visualize dataloader in a batch
   def visualize_data_in_batch(self, data_loader, start, end):
     classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -34,7 +46,7 @@ class CIFAR10DataOps:
     self.imshow(make_grid(images[start:end+1]), [classes[index] for index in labels.tolist()[start:end+1]])
 
 
-
+  # shpw images
   def imshow(self, img, labels):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
@@ -42,3 +54,10 @@ class CIFAR10DataOps:
     plt.title(labels)
     plt.show()
 
+# transforms class - used for albumentations
+class Transforms:
+    def __init__(self, transforms: albumentations.Compose):
+        self.transforms = transforms
+
+    def __call__(self, img, *args, **kwargs):
+        return self.transforms(image=np.array(img))
